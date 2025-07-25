8954b9ecf116c4ca0f19d19a0a4285302895ca0a
skavinash482 <massavinash28@gmail.com>
2025-07-25 10:30:09 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template
+import pickle
+app = Flask(__name__)
+# Load the model
+model1 =pickle.load(open(r'C:\Users\student\PycharmProjects\movie_avinash\data\model.pkl','rb'))
+@app.route('/')
+def home():
+    return render_template('index.html') # The form to input
+@app.route('/predict', methods=['POST'])
+def predict():
+ # Get input from HTML form
+    val1 = float(request.form['feature1'])
+    val2 = float(request.form['feature2'])
+    val3 = float(request.form['feature3'])
+    val4 = float(request.form['feature4'])
+    # Format input data for prediction
+    input_data = [[val1, val2], [val3, val4]] # Two users'
+    predictions = model1.predict(input_data)
+    return render_template('result.html', prediction=predictions)
+if __name__ == '__main__':
+ app.run(debug=True)
